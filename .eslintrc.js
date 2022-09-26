caf096be664a8cd9b3bbef80c50334a1e8d2a61c
Roman Zipp <ich@ich.wtf>
2022-09-26 13:46:59 +0200

    Replace action with node

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'airbnb-base',
+    ],
+    parserOptions: {
+        parser: 'babel-eslint',
+        sourceType: 'module',
+        ecmaVersion: 12,
+    },
+    rules: {
+        indent: ['error', 4],
+        'max-len': 'off',
+        'no-restricted-syntax': 'off',
+        'no-console': 'off',
+    },
+    globals: {
+        process: true,
+    },
+};
---
+++ index.js
@@ -0,0 +1,45 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const axios = require('axios');
+
+try {
+    const [endpoint, token, server, job, selector, version, cfAccessClientId, cfAccessClientSecret] = [
+        core.getInput('endpoint'),
+        core.getInput('token'),
+        core.getInput('server'),
+        core.getInput('job'),
+        core.getInput('selector'),
+        core.getInput('version'),
+        core.getInput('cf_access_client_id'),
+        core.getInput('cf_access_client_secret'),
+    ];
+
+    const headers = {
+        Authorization: `${token}`,
+        'Content-Type': 'application/json',
+    };
+
+    if (cfAccessClientId) {
+        headers['CF-Access-Client-Id'] = cfAccessClientId;
+        headers['CF-Access-Client-Secret'] = cfAccessClientSecret;
+    }
+
+    axios
+        .post(`${endpoint}/api/deploy`, {
+            server, job, selector, version,
+        }, { headers })
+        .then((res) => {
+            const data = JSON.parse(res.data);
+            const payload = JSON.stringify(github.context.payload, undefined, 2);
+
+            console.log('The event payload', payload);
+            console.log('response:', data);
+
+            core.setOutput('status', 'ok');
+        })
+        .catch((err) => {
+            core.setFailed(err.message);
+        });
+} catch (error) {
+    core.setFailed(error.message);
+}
